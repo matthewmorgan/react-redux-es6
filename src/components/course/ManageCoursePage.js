291c4b8f706bc0f1bc3090f26e04c616fcab40d7
Matthew Morgan <matt@dulcetsoftware.com>
2016-11-26 10:27:50 -0500

    Wire CourseForm

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,27 +2,47 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors: {}
+    };
   }
 
   render() {
     return (
-      <h1>Manage Course</h1>
+      <div>
+        <CourseForm
+          allAuthors={[]}
+          errors={this.state.errors}
+          course={this.state.course}/>
+      </div>
     );
   }
 }
 
 ManageCoursePage.propTypes = {
-  //myProp: PropTypes.object.isRequired,
+  course: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
+  let course = {
+    id:        '',
+    watchHref: '',
+    title:     '',
+    authorId:  '',
+    length:    '',
+    category:  ''
+  };
+
   return {
-    state: state
+    course: course
   };
 }
 
