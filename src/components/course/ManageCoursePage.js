aa32d58dd31bdca249162fa4cf57bd288b918006
Matthew Morgan <matt@dulcetsoftware.com>
2016-11-27 07:09:12 -0500

    Update page when props are loaded

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -17,6 +17,11 @@ class ManageCoursePage extends React.Component {
     this.saveCourse = this.saveCourse.bind(this);
   }
 
+  componentWillReceiveProps(nextProps){
+    if (this.props.course.id != nextProps.course.id){
+      this.setState({course: Object.assign({}, nextProps.course)});
+    }
+  }
 
   updateCourseState(event) {
     const field = event.target.name;
