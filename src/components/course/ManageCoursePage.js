889728ee54d6e34e4cb78976a0b54687247594e2
Matthew Morgan <matt@dulcetsoftware.com>
2016-11-27 09:42:45 -0500

    Add unit test for author mapping

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -4,6 +4,7 @@ import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
 import toastr from 'toastr';
+import {authorsFormattedForDropdown} from '../../selectors/selectors';
 
 
 export class ManageCoursePage extends React.Component {
@@ -108,16 +109,10 @@ function mapStateToProps(state, ownProps) {
   if (state.courses.length && courseId){
     course = getCourseById(state.courses, courseId);
   }
-  const authorsFormattedForDropdown = state.authors.map(a => {
-    return {
-      value: a.id,
-      text: `${a.firstName} ${a.lastName}`
-    };
-  });
 
   return {
     course: course,
-    authors: authorsFormattedForDropdown
+    authors: authorsFormattedForDropdown(state.authors)
   };
 }
 
