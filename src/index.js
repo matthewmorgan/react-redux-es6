214221d9f2213b3cf57a8ca5cd65aa91f3556c4c
Matthew Morgan <matt@dulcetsoftware.com>
2016-11-27 07:49:19 -0500

    Add toast on form save

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import {loadAuthors} from './actions/authorActions';
 import routes from './routes';
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());
