73696f7c68c4d8e5e8b632107951ec2842e46dc8
Matthew Morgan <matt@dulcetsoftware.com>
2016-11-30 05:49:22 -0500

    Make comments for clarification

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -10,16 +10,18 @@ export default(state = initialState.courses, action) =>
 
     case types.CREATE_COURSE_SUCCESS:
       return [
+        // grab our state, then add our new course in
         ...state,
         Object.assign({}, action.course)
       ];
 
     case types.UPDATE_COURSE_SUCCESS:
       return [
+        // filter out THIS course from our copy of the state, then add our updated course in
         ...state.filter(course => course.id !== action.course.id),
         Object.assign({}, action.course)
       ];
-    
+
     default:
       return state;
   }
