caaad6c6294fcc6ba2b78127b737845ec4abdae2
Matthew Morgan <matt@dulcetsoftware.com>
2016-11-30 05:57:46 -0500

    Add AuthorListRow

---
+++ src/components/author/AuthorListRow.js
@@ -0,0 +1,18 @@
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
+
+const AuthorListRow = ({author}) => {
+  return (
+    <tr>
+      <td><a href={author.watchHref} target="_blank">Watch</a></td>
+      <td><Link to={'/author/' + author.id}>{author.name}</Link></td>
+      <td>{author.authorId}</td>
+    </tr>
+  );
+};
+
+AuthorListRow.propTypes = {
+  author: PropTypes.object.isRequired
+};
+
+export default AuthorListRow;
